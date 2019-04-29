95ae90cf88a30b56fe62ad22a1da04429d78ea3e
Kevin Walton <kevywalton@gmail.com>
2019-04-29 10:51:29 +0000

    test not equal fail

--- byotest.py
+++ byotest.py
@@ -1,5 +1,9 @@
 def number_of_evens(numbers):
-    return 0
+    evens = 0
+    for num in numbers:
+        if num % 2 == 0:
+            evens += 1
+    return evens
 
 def test_are_equal(actual, expected):
     assert expected == actual, "Expected {0}, but got {1}".format(expected, actual)
@@ -14,3 +18,4 @@ def test_not_in(collection, item):
     assert item not in collection, "{0} does contain {1}".format(collection, item)
     
 test_are_equal(number_of_evens([1,2,3,4,5]), 2)
+test_not_equal(number_of_evens([2,4,5,8]), 3)
