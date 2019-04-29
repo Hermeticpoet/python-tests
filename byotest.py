9b69a68929ce6791583f3797fec049f44b07b594
Kevin Walton <kevywalton@gmail.com>
2019-04-29 10:52:00 +0000

    test not equal passed

--- byotest.py
+++ byotest.py
@@ -18,4 +18,4 @@ def test_not_in(collection, item):
     assert item not in collection, "{0} does contain {1}".format(collection, item)
     
 test_are_equal(number_of_evens([1,2,3,4,5]), 2)
-test_not_equal(number_of_evens([2,4,5,8]), 3)
+test_not_equal(number_of_evens([2,4,5,8]), 4)
